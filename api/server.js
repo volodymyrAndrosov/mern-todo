7f7023ea492e852b29d3603918887127bfd6b21b
volodymyrAndrosov <1999vovan@ukr.net>
2023-07-15 19:38:55 +0300

    add routes with tested logic

--- api/server.js
+++ api/server.js
@@ -1,7 +1,11 @@
+// default imports
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+//custom imports
+const Todo = require('./models/Todo');
 
+//main app
 const app = express();
 
 app.use(express.json());
@@ -14,4 +18,41 @@ mongoose.connect("mongodb://127.0.0.1:27017/mern-todo", {
     console.log("connected to DB ")
 }).catch(console.error);
 
-app.listen(3001, () => console.log("Server is stared on port 3001"))
+app.get('/todos', async (req, res) => {
+    const todos = await Todo.find();
+
+    res.json(todos);
+});
+
+app.post('/todo/new', (req, res) => {
+    const { text } = req.body;
+    const todo = new Todo({
+        text
+    });
+
+    todo.save();
+    res.json(todo);
+});
+
+//id is dynamic parameter URL
+app.delete('/todo/delete/:id', async (req, res) => {
+    const { id } = req.params;
+
+    const result = await Todo.findByIdAndDelete(id);
+
+    res.json(result);
+})
+
+app.put('/todo/complete/:id', async (req, res) => {
+    const { id } = req.params;
+
+    const todo = await Todo.findById(id);
+    todo.complete = !todo.complete;
+
+    todo.save();
+
+    res.json(todo);
+})
+
+
+app.listen(3001, () => console.log("Server is stared on port 3001"));
